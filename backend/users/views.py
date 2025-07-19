6fb23695c6c05e7fcefb4dbd379d225205589524
eve kibaara <kibaaraeve@gmail.com>
2025-07-19 21:37:34 +0100

    feat (users) : create placeholder views for register and login

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        # logic to create a user will go here... later !!
+        return JsonResponse({'message': 'Register view is working!'})
+    return JsonResponse({'error' : 'POST method required.'}, status = 400)
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        # logic to log in a user (do later cba rn!!)
+        return JsonResponse({'message' : 'login view is working!'})
+    return JsonResponse({'error' : 'POST method required.'}, status = 400)
